80ca364b71473d13a010aa75dfbd8b30bd9e9aad
Paulo Rodrigo <paulorodrigoba@outlook.com>
2024-09-24 01:07:21 -0400

    atualização da classe Cliente

--- src/main/java/br/com/bancodigitaldoamazonas/api/domain/cliente/Cliente.java
+++ src/main/java/br/com/bancodigitaldoamazonas/api/domain/cliente/Cliente.java
@@ -1,9 +1,7 @@
 package br.com.bancodigitaldoamazonas.api.domain.cliente;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import br.com.bancodigitaldoamazonas.api.domain.endereco.Endereco;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -12,9 +10,30 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
+
     private String cpf;
-    private String endereco;
+    private String rg;
+    private String nome;
+    private String email;
     private String telefone;
-    // Getters e Setters com Lombok
+
+    @Embedded
+    private Endereco endereco;
+
+    private Boolean ativo;
+
+    public Cliente (){}
+
+    public Cliente(DadosCadastroCliente dados){
+        this.ativo = true;
+        this.cpf = dados.cpf();
+        this.rg = dados.rg();
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.endereco = new Endereco(dados.endereco());
+
+    }
+
+
 }
